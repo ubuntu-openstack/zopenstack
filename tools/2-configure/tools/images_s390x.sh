38dfec963b02c6b463faf33c68b04dc5bc8191f9
james page <james.page@ubuntu.com>
2016-11-03 14:59:38 +0000

    copy configs stuff

---
+++ tools/2-configure/tools/images_s390x.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+# Download s390x images and add to glance.
+
+# Download images if not already present
+mkdir -p ~/images
+[ -f ~/images/xenial-server-cloudimg-s390x-disk1.img ] || {
+    wget -O ~/images/xenial-server-cloudimg-s390x-disk1.img http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-s390x-disk1.img
+}
+
+# XXX: s390x cirros image not yet available, required for tempest testing.
+#[ -f ~/images/cirros-d150923-ppc64le-disk.img ] || {
+#    wget -O ~/images/cirros-???.img http://download.cirros-cloud.net/daily/20150923/cirros-???.img
+#}
+
+
+# Upload glance images to overcloud
+glance --os-image-api-version 1 image-create --name="xenial-s390x" --is-public=true --progress \
+    --container-format=bare --disk-format=qcow2 < ~/images/xenial-server-cloudimg-s390x-disk1.img
+
+# Set image architecture properties
+glance --os-image-api-version 1 image-update --property architecture=s390x xenial-s390x
+
