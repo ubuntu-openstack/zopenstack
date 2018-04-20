67ba908d26ebbe5d78746d1fa217bc994ab02872
sfeole <sean.feole@canonical.com>
2018-04-20 07:52:19 -0400

    Add Bionic to -v3 image fetch
    Adding Bionic to the s390x image fetch script

--- tools/2-configure/tools/images_s390x-v3.sh
+++ tools/2-configure/tools/images_s390x-v3.sh
@@ -5,8 +5,13 @@
 mkdir -p ~/images
 [ -f ~/images/xenial-server-cloudimg-s390x-disk1.img ] || {
     wget -O ~/images/xenial-server-cloudimg-s390x-disk1.img http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-s390x-disk1.img
+
+[ -f ~/images/bionic-server-cloudimg-s390x-disk1.img ] || {
+    wget -O ~/images/bionic-server-cloudimg-s390x-disk1.img http://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-s390x-disk1.img
 }
 
 # Create images
 openstack image show xenial-s390x ||\
 	  openstack image create --public --container-format bare --disk-format qcow2 --file ~/images/xenial-server-cloudimg-s390x-disk1.img xenial-s390x
+openstack image show bionic-s390x ||\
+	  openstack image create --public --container-format bare --disk-format qcow2 --file ~/images/bionic-server-cloudimg-s390x-disk1.img bionic-s390x
