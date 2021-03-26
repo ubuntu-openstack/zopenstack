4cb883ffcf12f82736562cc78d319176947fe024
Aurelien Lourot <aurelien.lourot@canonical.com>
2021-03-26 15:52:50 +0100

    Adapt to newer OpenStack client CLI

--- tools/2-configure/tools/sec_groups.sh
+++ tools/2-configure/tools/sec_groups.sh
@@ -5,9 +5,9 @@ secgroup=${1:-`openstack security group list |grep default| awk '{print $2}'`}
 for group in $secgroup; do
 
     for port in 22 53 80 443; do
-        openstack security group rule create $group --proto tcp --src-ip 0.0.0.0/0 --dst-port $port ||:
+        openstack security group rule create $group --proto tcp --remote-ip 0.0.0.0/0 --dst-port $port ||:
     done
 
-    openstack security group rule create $group --proto icmp --src-ip 0.0.0.0/0 ||:
+    openstack security group rule create $group --proto icmp --remote-ip 0.0.0.0/0 ||:
 done
 
