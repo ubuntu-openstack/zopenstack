3232f3d2a72ccbda137f012f8d7a255b5ebaee48
Aurelien Lourot <aurelien.lourot@canonical.com>
2021-04-16 12:05:01 +0200

    LA_TEMP octavia

--- tools/2-configure/tools/sec_groups.sh
+++ tools/2-configure/tools/sec_groups.sh
@@ -4,7 +4,7 @@ secgroup=${1:-`openstack security group list |grep default| awk '{print $2}'`}
 
 for group in $secgroup; do
 
-    for port in 22 53 80 443; do
+    for port in 22 53 80 81 443 9443; do  # LA_TEMP
         openstack security group rule create $group --proto tcp --remote-ip 0.0.0.0/0 --dst-port $port ||:
     done
 
