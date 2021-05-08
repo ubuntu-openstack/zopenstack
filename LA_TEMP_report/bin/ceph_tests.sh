1a81ba195ee39f2fc0fafd039ffb6d6c01a8a8d3
Aurelien Lourot <aurelien.lourot@canonical.com>
2021-05-08 12:18:39 +0200

    LA_TEMP ceph-only report
    # Conflicts:
    #	LA_TEMP_report/report/catalog_list.txt
    #	LA_TEMP_report/report/ceph_tests.txt
    #	LA_TEMP_report/report/hypervisor_list.txt
    #	LA_TEMP_report/report/image_list.txt
    #	LA_TEMP_report/report/instance_launch.txt
    #	LA_TEMP_report/report/instance_ssh.txt
    #	LA_TEMP_report/report/juju_status.txt
    #	LA_TEMP_report/report/network_agent_list.txt
    #	LA_TEMP_report/report/network_extension_list.txt
    #	LA_TEMP_report/report/network_list.txt
    #	LA_TEMP_report/report/openstack_origin.txt
    #	LA_TEMP_report/report/tempest_smoke.txt

--- LA_TEMP_report/bin/ceph_tests.sh
+++ LA_TEMP_report/bin/ceph_tests.sh
@@ -4,5 +4,5 @@ set -xe
 
 juju run -u ceph-mon/leader -- sudo ceph osd lspools
 juju run -u ceph-mon/leader -- sudo ceph osd df
-juju run -u ceph-mon/leader -- sudo rados -p cinder-ceph ls
-juju run -u ceph-mon/leader -- sudo rados -p glance ls
+#LA_TODO juju run -u ceph-mon/leader -- sudo rados -p cinder-ceph ls
+#LA_TODO juju run -u ceph-mon/leader -- sudo rados -p glance ls
--- LA_TEMP_report/test_and_report.sh
+++ LA_TEMP_report/test_and_report.sh
@@ -6,19 +6,19 @@ set -x
 rm -rf report/
 mkdir report/
 
-bin/tempest_smoke.sh > report/tempest_smoke.txt 2>&1
+#LA_TODO bin/tempest_smoke.sh > report/tempest_smoke.txt 2>&1
 
-. ../openrcv3_domain
+#LA_TODO . ../openrcv3_domain
 bin/juju_status.sh > report/juju_status.txt 2>&1
-bin/hypervisor_list.sh > report/hypervisor_list.txt 2>&1
-bin/network_agent_list.sh > report/network_agent_list.txt 2>&1
-bin/network_extension_list.sh > report/network_extension_list.txt 2>&1
-bin/network_list.sh > report/network_list.txt 2>&1
-bin/image_list.sh > report/image_list.txt 2>&1
+#LA_TODO bin/hypervisor_list.sh > report/hypervisor_list.txt 2>&1
+#LA_TODO bin/network_agent_list.sh > report/network_agent_list.txt 2>&1
+#LA_TODO bin/network_extension_list.sh > report/network_extension_list.txt 2>&1
+#LA_TODO bin/network_list.sh > report/network_list.txt 2>&1
+#LA_TODO bin/image_list.sh > report/image_list.txt 2>&1
 bin/ceph_tests.sh > report/ceph_tests.txt 2>&1
 bin/openstack_origin.sh > report/openstack_origin.txt 2>&1
 
-. ../openrcv3_project
-bin/catalog_list.sh > report/catalog_list.txt 2>&1
-bin/instance_launch.sh > report/instance_launch.txt 2>&1
-bin/instance_ssh.sh > report/instance_ssh.txt 2>&1
+#LA_TODO . ../openrcv3_project
+#LA_TODO bin/catalog_list.sh > report/catalog_list.txt 2>&1
+#LA_TODO bin/instance_launch.sh > report/instance_launch.txt 2>&1
+#LA_TODO bin/instance_ssh.sh > report/instance_ssh.txt 2>&1
