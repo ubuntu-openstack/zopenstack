1aed905cfcc7696b02f88ac61f2ed697569bb2a1
Felipe Reyes <felipe.reyes@canonical.com>
2022-04-22 23:58:54 -0400

    do-release-upgrade.sh: Update host keys

--- tools/1-deploy/do-release-upgrade.sh
+++ tools/1-deploy/do-release-upgrade.sh
@@ -2,6 +2,8 @@
 
 readarray LPARS < ./lpars
 for LPAR in ${LPARS[@]}; do
+  ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R "${LPAR}"
+  ssh-keyscan -t rsa -H ${LPAR} >> $HOME/.ssh/known_hosts
   # upgrade from focal to jammy needs `-d` until jammy is GA.
   if [ "x$(ssh ubuntu@${LPAR} -- lsb_release -c -s)" != "xjammy" ]; then
     if (( $(distro-info --series=jammy --days) > 0 )); then
