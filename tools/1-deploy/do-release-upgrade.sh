0cd6d0b28a0c28c668ce34d7b36c8732732ed2a2
Felipe Reyes <felipe.reyes@canonical.com>
2022-04-25 12:39:24 -0400

    Use meta-release-tls instead of distro-info
    meta-release-tls doesn't get updated until the first point release which
    is what do-release-upgrade to determine if there is an upgrade
    available.

--- tools/1-deploy/do-release-upgrade.sh
+++ tools/1-deploy/do-release-upgrade.sh
@@ -6,7 +6,11 @@ for LPAR in ${LPARS[@]}; do
   ssh-keyscan -t rsa -H ${LPAR} >> $HOME/.ssh/known_hosts
   # upgrade from focal to jammy needs `-d` until jammy is GA.
   if [ "x$(ssh ubuntu@${LPAR} -- lsb_release -c -s)" != "xjammy" ]; then
-    if (( $(distro-info --series=jammy --days) > 0 )); then
+    # The -d flag is needed not until the LTS version is released, instead
+    # until the first point release of the LTS is released, that milestone is
+    # reflected on the meta-release-lts file which is consumed by
+    # do-release-upgrade to determine if there is a new version available.
+    if ! curl -s "http://changelogs.ubuntu.com/meta-release-lts" | grep -q "Dist: jammy"; then
       EXTRA_OPTS="-d"
     else
       EXTRA_OPTS=""
