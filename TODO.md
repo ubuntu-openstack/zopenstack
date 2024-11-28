a3ff4e43968bf8020d74135ed58117351d30b700
James Page <james.page@canonical.com>
2024-11-28 11:39:06 +0000

    Use tempest snap for testing
    Switch to using tempest snap and discovery mechanism for running
    functional tests.
    
    Provide default exclusion list for admin tests that fail at
    Dalmatian due to changes in enforcement of scope that need to
    be debugged.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # TODO
 
-- Use tempest from snap, configure using tempest.discover
 - Use openstackclients from snap
 - Move to using terraform for configuration
 - Refresh generally for running on 24.04
+- Debug failing tempest admin tests at Dalmatian
--- report/bin/tempest_smoke.sh
+++ report/bin/tempest_smoke.sh
@@ -2,5 +2,15 @@
 
 set -xe
 
-cd ../tools/2-configure/tempest/
-env https_proxy="http://squid.internal:3128" tox -e smoke
+
+tempest.discover \
+    --image https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-s390x.img \
+    --flavor-min-disk 10 \
+    validation.image_ssh_user ubuntu \
+    validation.image_alt_ssh_user ubuntu \
+    compute.flavor_ref 2 \
+    compute.flavor_ref_alt 3 \
+    auth.admin_domain_name admin_domain
+
+
+tempest run --smoke --concurrency 8 --config-file etc/tempest.conf --exclude-list etc/exclude-list.txt
